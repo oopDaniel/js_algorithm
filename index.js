82742619f667943a477446656cd0f9e5e9d6d0c5
Daniel <oopdaniel@gmail.com>
2018-10-12 15:26:57 -0700

    add text content

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import fs from 'fs'
 import { clock, printTime } from './utils'
 import solve from './solve'
 
-const now = clock()
-
 const FILE_NAME_IN = 'input.txt'
 const file = fs.readFileSync(`./${FILE_NAME_IN}`, 'utf8')
-const arr = file.split(', ').map(c => +c)
+const lines = file.split('\n')
+const keys = lines[0].split(', ')
+const values = lines[1].split(', ').map(c => +c)
+const pairs = keys.map((key, i) => ({ key, value: values[i] }))
 
-solve(arr)
+const now = clock()
+
+solve(pairs)
 
 printTime(clock(now))
-
-console.log('Result: ', arr)
